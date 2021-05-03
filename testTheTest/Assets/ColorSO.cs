dca121279e6df0e57c4649a96776df4f8a6e96d1
Logan Smith <lmaxsmith@gmail.com>
2021-05-03 12:54:06 -0700

    Initial file setup.

---
+++ testTheTest/Assets/ColorSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "colorData", menuName = "ArgyleTest/ColorButton", order = 1)]
+public class ColorSO : ScriptableObject
+{
+    public string colorName;
+    public Color color;
+
+
+}
